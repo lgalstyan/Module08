7f84862eff6b621ba1a5042c5c6b3a20536b9f78
Larisa Galstyan <lgalstya@c2r17s1.42yerevan.am>
2023-07-18 17:44:51 +0400

    Done ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,5 +1,8 @@
-#ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+#pragma one
+#define RESET	"\033[0m"
+#define BLUE	"\033[1;34m"
+#define GREEN   "\x1B[32m"
+#define RED		"\033[31m"
 
 #include <iostream>
 #include <algorithm>
@@ -9,14 +12,19 @@ class CantFindException : public std::exception
     public:
         const char * what() const throw()
         {
-            return ("Exception: Can not find!");
+            return ("Exception: value not found in the container!!");
         }
 };
 
 template <typename T>
-void easyfind(T type, int i)
+void easyfind(const T& array, int find)
 {
-
+    typename T::const_iterator iter;
+    iter = std::find(array.begin(), array.end(), find);
+    if (iter == array.end())
+    {
+        throw CantFindException();
+        return ;
+    }
+        std::cout << GREEN << "Found the first occurrence of " << BLUE << *iter << RESET << std::endl;
 }
-
-#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,40 @@
 #include "easyfind.hpp"
+#include <vector>
+#include <array>
 
 int main()
 {
-    
-    return (0);
+    // Example usage with vector
+    std::array<int, 5> arr = {1, 3, 5, 7, 9};
+    std::vector<int> vec;
+    vec.push_back(2);
+    vec.push_back(4);
+    vec.push_back(6);
+    vec.push_back(8);
+
+    try
+    {
+        easyfind(vec, 2);
+    }
+    catch (const std::exception& e) {
+        std::cout << RED << e.what() << RESET << std::endl;
+    }
+
+    try
+    {
+        easyfind(arr, 5);
+    }
+    catch (const std::exception& e) {
+        std::cout << RED << e.what() << RESET << std::endl;
+    }
+
+    try
+    {
+        easyfind(vec, 10);
+    }
+    catch (const std::exception& e) {
+        std::cout << RED << e.what() << RESET << std::endl;
+    }
+
+    return 0;
 }
