1b302b0ba779b1fdfc8158eb1435cc5d28f923b6
Larisa Galstyan <lgalstya@c2r17s1.42yerevan.am>
2023-07-23 16:39:57 +0400

    add any tests

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,6 +1,11 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
+#define RESET	"\033[0m"
+#define BLUE	"\033[1;34m"
+#define GREEN   "\x1B[32m"
+#define RED		"\033[31m"
+
 #include <iostream>
 #include <stack>
 #include <deque>
@@ -11,35 +16,40 @@ class MutantStack : public std::stack<T, Container>
     public:
         typedef typename Container::iterator iterator;
         typedef typename Container::iterator const_iterator;
-        // MutantStack() : std::stack<T, Container>() {} 
-        MutantStack() {} 
+
+        MutantStack() : std::stack<T, Container>() {} 
+        
         MutantStack<T, Container>(const MutantStack<T, Container>& other)
         {
             *this = other;
         }
+
         MutantStack<T, Container>& operator=(const MutantStack<T, Container>& rhs)
         {
             if (this != &rhs)
                 this->c = rhs.c;
             return (*this);
         }
+
         iterator begin()
         {
             return this->c.begin();
         }
+
         iterator end()
         {
             return this->c.end();
         }
+
         const_iterator begin() const
         {
             return this->c.begin();
         }
+
         const_iterator end() const
         {
             return this->c.end();
         }
-
 };
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,25 +2,78 @@
 
 int main()
 {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
+    std::cout << GREEN << "-----------------subject test---------------\n" << RESET;
     {
-        std::cout << *it << std::endl;
+        MutantStack<int> mstack;
+        mstack.push(5);
+        mstack.push(17);
+        std::cout << mstack.top() << std::endl;
+        mstack.pop();
+        std::cout << mstack.size() << std::endl;
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(737);
+        mstack.push(0);
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
         ++it;
+        --it;
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::stack<int> s(mstack);
     }
-    std::stack<int> s(mstack);
+    
+    std::cout << GREEN << "-----------------My first test---------------\n" << RESET;
+    {
+        int multiply = 10;
+        MutantStack<int> stack;
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        std::cout << "Stack is: ";
+        for (MutantStack<int>::iterator it = stack.begin(); it != stack.end(); ++it)
+        {
+            std::cout << *it << " ";
+        }
+        for (MutantStack<int>::iterator it = stack.begin(); it != stack.end(); ++it)
+        {
+            *it *= multiply;
+            *it += 1;
+        }
+
+        std::cout << "\nStack elements after modification: ";
+        for (MutantStack<int>::iterator it = stack.begin(); it != stack.end(); ++it)
+        {
+            std::cout << *it << " ";
+        }
+            std::cout << std::endl;
+    }
+
+    std::cout << GREEN << "-----------------My second test---------------\n" << RESET;
+    {
+        MutantStack<double> doubleStack;
+
+        doubleStack.push(3.14);
+        doubleStack.push(1.618);
+        doubleStack.push(2.718);
+        doubleStack.push(0.577);
+
+        std::cout << "Stack elements using const_iterator: ";
+        for (MutantStack<double>::const_iterator cit = doubleStack.begin(); cit != doubleStack.end(); ++cit) {
+            std::cout << *cit << " "; 
+        }
+        std::cout << std::endl;
+
+
+        MutantStack<double> copyStack = doubleStack;
+        doubleStack.pop();
+
+        std::cout << "Original stack size: " << doubleStack.size() << std::endl;
+        std::cout << "Copy stack size: " << copyStack.size() << std::endl;
+
+    }
     return 0;
 }
