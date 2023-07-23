12fa86777c51b92f4a350280670c2211112a35ee
Larisa Galstyan <lgalstya@c2r17s1.42yerevan.am>
2023-07-23 15:22:02 +0400

    dont work

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,19 +1,43 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-template <typename T>
-class MutantStack : public std::stack
+#include <iostream>
+#include <stack>
+
+template <typename T, typename  Cont>
+class MutantStack : public std::stack<T, Cont>
 {
     public:
-        MutantStack();
-        MutantStack(const MutantStack& );
-        MutantStack& operator=(const MutantStack& );
-        ~MutantStack();
-        void push();
-        void pop();
-        T top();
-        T begin();
-        T end();
+        MutantStack() : std::stack<T, Cont> {}
+        MutantStack<T, Cont>(const MutantStack<T, Cont>& other)
+        {
+            *this = other;
+        }
+        MutantStack<T, Cont>& operator=(const MutantStack<T, Cont>& rhs)
+        {
+            if (this != &rhs)
+                this->c = rhs.c;
+            return (*this);
+        }
+        ~MutantStack<T, Cont>(){}
+        typedef typename std::stack<T, Cont>::container_type::iterator iter;
+        typedef typename std::stack<T, Cont>::container_type::iterator const_iter;
+        iter begin()
+        {
+            return this->c.begin();
+        }
+        iter end()
+        {
+            return this->c.end();
+        }
+        const_iter begin() const
+        {
+            return this->c.begin();
+        }
+        const_iter end() const
+        {
+            return this->c.end();
+        }
 
 };
 
